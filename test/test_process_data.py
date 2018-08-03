5ee5a52cef4de713d22d8a00033b835fd5bf0f7a
Yacin Tmimi <yacintmimi@gmail.com>
2018-08-03 17:36:36 -0400

    added new tests to process_data.py

--- test/test_process_data.py
+++ test/test_process_data.py
@@ -1,175 +1,30 @@
 import os
 import sys
 import re
-import random
 
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(path, 'chess_com'))
 
+from chess_api import chess_com_data
 import process_data
-
+import settings
 import unittest
 
 
 
-class Test_base_re(unittest.TestCase):
-	def test_file(self):
-		for file in 'abcdefgh':
-			self.assertTrue(re.match(process_data.FILE, file))
-
-	def test_rank(self):
-		for rank in range(1, 9):
-			self.assertTrue(re.match(process_data.RANK, f'{rank}'))
-
-	def test_square(self):
-		for letter in 'abcdefgh':
-			for i in range(1,9):
-				square = f'{letter}{i}'
-				self.assertTrue(re.match(process_data.SQUARE, square))
-
-
-
-class Test_basic_moves(unittest.TestCase):
-	def test_promote(self):
-		for letter in 'abcdefgh':
-			for piece in 'QRNB':
-				for num in [1,8]:
-					promote = f'{letter}{num}={piece}'
-					self.assertTrue(re.match(process_data.PROMOTE, promote))
-
-	def test_piece_move(self):
-		for letter in 'abcdefgh':
-			for piece in 'KQRNB':
-				for num in range(1, 9):
-					move = f'{piece}{letter}{num}'
-					self.assertTrue(re.match(process_data.PIECE_MOVE, move ))
-
-	def test_pawn_move(self):
-		for letter in 'abcdefgh':
-			for num in range(2,8):
-				move = f'{letter}{num}'
-				self.assertTrue(re.match(process_data.PAWN_MOVE, move ))
-
-	def test_king_side_castle(self):
-		self.assertTrue(re.match(process_data.KING_CASTLE, 'O-O'))
-
-	def test_queen_side_castle(self):
-		self.assertTrue(re.match(process_data.QUEEN_CASTLE, 'O-O-O'))
-
-
-
-class Test_basic_captures(unittest.TestCase):
-	def test_peice_capture(self):
-		for file in 'abcdefgh':
-			for piece in 'KQRNB':
-				for num in range(1,9):
-					cap = f'{piece}x{file}{num}'
-					self.assertTrue(re.match(process_data.PIECE_CAPTURE, cap))
-
-	def test_pawn_capture(self):
-		files = 'abcdefgh'
-		for i, file in enumerate(files):
-			for num in range(1,9):
-				if file !='h':
-					cap = f'{file}x{files[i+1]}{num}'
-					self.assertTrue(re.match(process_data.PAWN_CAPTURE, cap))
-		for i, file in enumerate(file[::-1]):
-			for num in range(1,9):
-				if file !='a':
-					cap = f'{file}x{files[i+1]}{num}'
-					self.assertTrue(re.match(process_data.PAWN_CAPTURE, cap))
-
-
-class Test_Ambiguous_moves_and_captures(unittest.TestCase):
-	def test_piece_file_move(self):
-		'''test ambigous moves, where more clarification is needed by adding the file
-		not all  moves test may be legal, but its important to test for the general patter
-		to make sure our regular expression can catch these moves
-		'''
-		for piece in 'QRNB':
-			for num in range(1,9):
-				move = f'{piece}ab{num}'
-				self.assertTrue(re.match(process_data.PIECE_FILE_MOVE, move))
-
-	def test_piece_rank_move(self):
-		'''test ambigous moves, where more clarification is needed by adding the rank
-		not all  moves test may be legal, but its important to test for the general patter
-		to make sure our regular expression can catch these moves
-		'''
-		for peice in 'QRNB':
-			for num in range(1,9):
-				move = f'{peice}2f{num}'
-				self.assertTrue(re.match(process_data.PIECE_RANK_MOVE, move))
-
-	def test_piece_file_rank_move(self):
-		'''test ambigous moves, where more clarification is needed by adding the file and rank
-		not all moves test may be legal, but its important to test for the general patter
-		to make sure our regular expression can catch these moves
-		'''
-		for peice in 'QRNB':
-			for num in range(1,9):
-				move = f'{peice}d2f{num}'
-				self.assertTrue(re.match(process_data.PIECE_FILE_RANK_MOVE, move))
-
-	def test_piece_file_capture(self):
-		'''test ambigous moves, where more clarification is needed by adding the file
-		not all  moves test may be legal, but its important to test for the general patter
-		to make sure our regular expression can catch these moves
-		'''
-		for piece in 'QRNB':
-			for num in range(1,9):
-				move = f'{piece}axb{num}'
-				self.assertTrue(re.match(process_data.PIECE_FILE_CAPTURE, move))
-
-	def test_piece_rank_capture(self):
-		'''test ambigous moves, where more clarification is needed by adding the rank
-		not all  moves test may be legal, but its important to test for the general patter
-		to make sure our regular expression can catch these moves
-		'''
-		for peice in 'QRNB':
-			for num in range(1,9):
-				move = f'{peice}2xf{num}'
-				self.assertTrue(re.match(process_data.PIECE_RANK_CAPTURE, move))
-
-
-
 class Test_Check_and_Checkmate(unittest.TestCase):
-	'''
-	class to test that if a move is made and it comes with check or checkmate
-	that it is recognized by the appropriate regular expression
-	'''
 	@classmethod
 	def setUpClass(cls):
-		#examples of each kind of move/capture
-		cls.promote = 'h1=Q'
-		cls.piece_move = 'Qb3'
-		cls.pawn_move = 'e4'
-		cls.piece_file_move = 'Ndf5'
-		cls.piece_rank_move = 'N6f4'
-		cls.piece_file_rank_move = 'Nb3d4'
-		cls.piece_capture = 'Bxf7'
-		cls.pawn_capture = 'exd4'
-		cls.piece_file_capture = 'Rfxd8'
-		cls.piece_rank_capture = 'N6xd5'
-		cls.king_castle = 'O-O'
-		cls.queen_castle = 'O-O-O'
-		cls.example_list = [
-			cls.promote, cls.piece_move, cls.pawn_move, cls.piece_file_move,
-			cls.piece_rank_move, cls.piece_file_rank_move, cls.piece_capture,
-			cls.pawn_capture, cls.piece_file_capture, cls.piece_rank_capture,
-			cls.king_castle, cls.queen_castle
-		]
 		cls.re_list = [
-			process_data.PROMOTE, process_data.PIECE_MOVE, process_data.PAWN_MOVE, process_data.PIECE_FILE_MOVE,
-			process_data.PIECE_RANK_MOVE, process_data.PIECE_FILE_RANK_MOVE, process_data.PIECE_CAPTURE,
-			process_data.PAWN_CAPTURE, process_data.PIECE_FILE_CAPTURE, process_data.PIECE_RANK_CAPTURE,
-			process_data.KING_CASTLE, process_data.QUEEN_CASTLE
+			settings.PROMOTE, settings.PIECE_MOVE, settings.PAWN_MOVE, settings.PIECE_FILE_MOVE,
+			settings.PIECE_RANK_MOVE, settings.PIECE_FILE_RANK_MOVE, settings.PIECE_CAPTURE,
+			settings.PAWN_CAPTURE, settings.PIECE_FILE_CAPTURE, settings.PIECE_RANK_CAPTURE,
+			settings.KING_CASTLE, settings.QUEEN_CASTLE
 		]
-
-	def test_check_checkmate_move_function(self):
-		for re_str in self.re_list:
-			result = process_data.check_checkmate_move(re_str)
-			self.assertEqual(result, fr'{re_str}\+|{re_str}#|{re_str}')
+		#examples of each kind of move/capture based on the order of cls.re_list
+		cls.example_list = [
+			'h1=Q', 'Qb3', 'e4', 'Ndf5', 'N6f4', 'Nb3d4', 'Bxf7',
+			'exd4', 'Rfxd8', 'N6xd5', 'O-O', 'O-O-O',]
 
 	def test_check_checkmat_move(self):
 		for re_str, move in zip(self.re_list, self.example_list):
@@ -215,6 +70,63 @@ class Test_Find_Moves(unittest.TestCase):
 		self.assertEqual(move_dict, self.timestamp_dict)
 		self.assertEqual(len(move_dict.keys()), 51)
 
+
+
+class Test_Format_Game_Data_mock(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		cls.game_data = {   
+			'url': 'https://www.chess.com/live/game/{game number}',
+			'pgn': """[Event "Live Chess"]\n[Site "Chess.com"]\n[Date "2018.07.28"]\n[Round "-"]\n[White "username1"]\n[Black "username2"]\n[Result "0-1"]\n[ECO "B23"]\n[ECOUrl "https://www.chess.com/openings/{variation}"]\n[WhiteElo "1466"]\n[BlackElo "1402"]\n[TimeControl "600"]\n[Termination "username2 won by checkmate"]\n[StartTime "02:57:44"]\n[EndDate "2018.07.28"]\n[EndTime "02:57:44"]\n[Link "https://www.chess.com/live/game/{game number}"]\n\n1. e4 {[%clk 0:09:59.2]} 1... c5 {[%clk 0:09:57.1]} 2. Nc3 {[%clk 0:09:55.8]} 2... Nc6 {[%clk 0:09:55.7]} 3. Nf3 {[%clk 0:09:52.2]} 3... d6 {[%clk 0:09:53.7]} 4. d4 {[%clk 0:09:46.7]} 4... cxd4 {[%clk 0:09:50]} 5. Nxd4 {[%clk 0:09:44.8]} 5... Nf6 {[%clk 0:09:14.1]} 6. Bb5 {[%clk 0:09:41.6]} 6... Bd7 {[%clk 0:08:10.7]} 7. O-O {[%clk 0:09:29.9]} 7... Qb6 {[%clk 0:08:05]} 8. Be3 {[%clk 0:09:13.6]} 8... Ng4 {[%clk 0:07:57.1]} 9. Qxg4 {[%clk 0:09:10.1]} 9... Bxg4 {[%clk 0:07:52.6]} 10. Ne6 {[%clk 0:09:02.3]} 10... Qxe3 {[%clk 0:07:30]} 11. fxe3 {[%clk 0:08:45.6]} 11... fxe6 {[%clk 0:07:28.6]} 12. Rad1 {[%clk 0:08:38.9]} 12... Bxd1 {[%clk 0:07:14.7]} 13. Rxd1 {[%clk 0:08:36.6]} 13... O-O-O {[%clk 0:07:09.3]} 14. Bc4 {[%clk 0:08:04.1]} 14... e5 {[%clk 0:06:38.9]} 15. Be6+ {[%clk 0:07:36.6]} 15... Kb8 {[%clk 0:06:34.5]} 16. g4 {[%clk 0:07:23.9]} 16... g6 {[%clk 0:06:31.3]} 17. Nb5 {[%clk 0:07:21.3]} 17... Bh6 {[%clk 0:06:23.5]} 18. Re1 {[%clk 0:07:03.1]} 18... Rhf8 {[%clk 0:06:19.6]} 19. h4 {[%clk 0:06:51.9]} 19... Rf6 {[%clk 0:06:16.8]} 20. Bd5 {[%clk 0:06:46.9]} 20... g5 {[%clk 0:06:09.6]} 21. hxg5 {[%clk 0:06:42.5]} 21... Bxg5 {[%clk 0:06:08]} 22. Nc3 {[%clk 0:06:36.1]} 22... Nb4 {[%clk 0:05:45]} 23. Bb3 {[%clk 0:06:15.8]} 23... Rdf8 {[%clk 0:05:37.9]} 24. a3 {[%clk 0:06:12]} 24... Na6 {[%clk 0:05:12.8]} 25. Re2 {[%clk 0:05:47.8]} 25... Nc5 {[%clk 0:05:00.8]} 26. Nd5 {[%clk 0:05:45.1]} 26... Rh6 {[%clk 0:04:40.4]} 27. Rh2 {[%clk 0:05:22.9]} 27... Rxh2 {[%clk 0:04:23.6]} 28. Kxh2 {[%clk 0:05:19.7]} 28... Nxe4 {[%clk 0:04:21.3]} 29. Nc3 {[%clk 0:04:53.4]} 29... Nxc3 {[%clk 0:04:15.6]} 30. bxc3 {[%clk 0:04:52.1]} 30... Bxe3 {[%clk 0:04:13.5]} 31. c4 {[%clk 0:04:49.3]} 31... Bc5 {[%clk 0:04:06.1]} 32. Ba2 {[%clk 0:04:45.8]} 32... e4 {[%clk 0:04:02.7]} 33. Bb1 {[%clk 0:04:40.7]} 33... e3 {[%clk 0:04:01.6]} 34. c3 {[%clk 0:04:39.5]} 34... e2 {[%clk 0:04:00.9]} 35. Be4 {[%clk 0:04:33.5]} 35... e1=Q {[%clk 0:03:54.4]} 36. Bf5 {[%clk 0:04:31.9]} 36... Rxf5 {[%clk 0:03:42.8]} 37. gxf5 {[%clk 0:04:30.5]} 37... Qh4+ {[%clk 0:03:41]} 38. Kg2 {[%clk 0:04:29.1]} 38... Qg4+ {[%clk 0:03:39.9]} 39. Kf1 {[%clk 0:04:27.6]} 39... Qxf5+ {[%clk 0:03:38.6]} 40. Ke2 {[%clk 0:04:26.3]} 40... h5 {[%clk 0:03:32.1]} 41. Kd1 {[%clk 0:04:22.6]} 41... h4 {[%clk 0:03:31.3]} 42. Kc1 {[%clk 0:04:20.5]} 42... h3 {[%clk 0:03:30]} 43. Kb2 {[%clk 0:04:19.7]} 43... Bxa3+ {[%clk 0:03:08]} 44. Ka2 {[%clk 0:04:18.6]} 44... Bc1 {[%clk 0:03:02.5]} 45. Kb3 {[%clk 0:04:16.1]} 45... d5 {[%clk 0:02:45.4]} 46. cxd5 {[%clk 0:04:13.7]} 46... Qxd5+ {[%clk 0:02:44]} 47. Kc2 {[%clk 0:04:12.7]} 47... h2 {[%clk 0:02:42.3]} 48. Kxc1 {[%clk 0:04:11.3]} 48... h1=Q+ {[%clk 0:02:41.1]} 49. Kc2 {[%clk 0:04:10.3]} 49... a5 {[%clk 0:02:37.7]} 50. Kb2 {[%clk 0:04:08.1]} 50... a4 {[%clk 0:02:36]} 51. Ka3 {[%clk 0:04:07.6]} 51... Qb3# {[%clk 0:02:34.4]} 0-1""",
+			'time_control': '600', 'end_time': 1532746664, 'rated': True, 'fen': '1k6/1p2p3/8/8/p7/KqP5/8/7q w - -', 
+			'time_class': 'blitz', 'rules': 'chess', 
+			'white': {
+				'rating': 1432, 
+				'result': 'checkmated',
+				'@id':'https://api.chess.com/pub/player/username1',
+				'username':'username1'
+			}, 
+			'black': {
+				'rating': 1402, 
+				'result': 'win', 
+				'@id': 'https://api.chess.com/pub/player/username2',
+				'username': 'username2'
+			},
+		}
+
+	def test_pgn_to_dict(self):
+		expected_keys = [
+			'Event', 'Site', 'Date', 'Round', 'White', 'Black', 'Result', 'ECO',
+			'ECOUrl', 'WhiteElo', 'BlackElo', 'TimeControl', 'Termination',
+			'StartTime', 'EndDate', 'EndTime', 'Link', 'Moves', 'Variation',]
+		pgn_dict = process_data.pgn_to_dict(self.game_data['pgn'])
+		self.assertIsInstance(pgn_dict, dict)
+		for key in expected_keys:
+			self.assertIn(key, pgn_dict.keys())
+
+	def test_variatoin_from_url(self):
+		example_var_url = 'https://www.chess.com/openings/C42-Petrovs-Defense-Three-Knights-Game'
+		result = process_data.variation_from_url(example_var_url)
+		self.assertEqual(result, 'Petrovs Defense Three Knights Game')
+
+	def test_player_nationality(self):
+		nationality = {'some_url/US':'United States'}
+		player = {'@id':'some_url/US'}
+		player = process_data.player_nationality(settings.COUNTRY_CODES, nationality, player)
+		self.assertIn('nationality', player.keys())
+		self.assertEqual(player['nationality'], 'United States')
+
+	@unittest.skipIf(not settings.LIVE_TEST, 'Set LIVE_TEST to True in settings.py to run these test')
+	def test_player_nationality_live(self):
+		player_enpoint = f'https://api.chess.com/pub/player/{settings.USERNAME}' 
+		#ENSURE THAT country IS THE SAME COUNTRY ASSOCIATED WITH THE USERNAME
+		country = settings.COUNTRY_CODES[chess_com_data(player_enpoint)['country'].split('/')[-1]]
+		nationality = {}
+		player = {'@id':player_enpoint}
+		player = process_data.player_nationality(settings.COUNTRY_CODES, nationality, player)
+		self.assertIn('nationality', player.keys())
+		self.assertEqual(player['nationality'], country)
+		self.assertEqual(nationality[player_enpoint], country)
 
 
 
